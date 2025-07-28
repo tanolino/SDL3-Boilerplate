c43982927547ea84d94b5f81a15d8024cfcb2737
Nathanael Krauße <taniga_danae@gmx.de>
2025-07-28 22:18:50 +0200

    [ADD] jsmn - aka Jasmin JSON parser

---
+++ src/header_only_include.c
@@ -0,0 +1,9 @@
+#include <jsmn.h>
+
+#define NATE_DYNMEMORY_IMPLEMENTATION
+#include <nate_DynMemory.h>
+
+#define NATE_TASK_IMPLEMENTATION
+#define NATE_TASK_SDL3_THREAD
+#include <SDL3/SDL.h>
+#include <nate_Task.h>
