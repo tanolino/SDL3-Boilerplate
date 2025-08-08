43c270d9573a08634ee235fd22da2503d762d3eb
Nathanael Krauße <taniga_danae@gmx.de>
2025-08-08 12:14:05 +0200

    [FIX] Multiple Definition build error

--- src/main.c
+++ src/main.c
@@ -2,14 +2,6 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-
-#define NATE_DYNMEMORY_IMPLEMENTATION
-#include <nate_DynMemory.h>
-
-#define NATE_TASK_IMPLEMENTATION
-#define NATE_TASK_SDL3_THREAD
-#include <nate_Task.h>
-
 #include "main.h"
 
 // Global shared state
